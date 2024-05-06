802c14ca1e9be60f602f64c3611bf29a41b82ce7
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2024-05-06 16:28:32 +0600

    updated

--- src/app/(routes)/(home)/order.tsx
+++ src/app/(routes)/(home)/order.tsx
@@ -6,11 +6,11 @@ import Link from "next/link"
 
 export default function Order() {
           return (
-                    <div className="flex flex-col justify-center items-center w-[90%] md:w-2/3 mx-auto rounded-lg my-10 py-10 md:py-16 space-y-3 border bg-secondary">
+                    <div className="flex flex-col justify-center items-center w-[90%] md:w-2/3 mx-auto rounded-lg my-5 sm:my-10 py-10 md:py-16 space-y-3 border bg-secondary">
                               <h3 className="text-lg">Welcome to</h3>
                               <h1 className="text-4xl font-bold text-primary">{Config.title}</h1>
                               <p className="text-lg">Please Select</p>
-                              <div className="flex flex-row space-x-6">
+                              <div className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-6">
                                         <Link href="/menu" className={buttonVariants({ size: "sm" })}>
                                                   Order Online
                                         </Link>
