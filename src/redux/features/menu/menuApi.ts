afefe94f4fe3b327a00e9cd1a2eeeaa5e23589da
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2024-05-07 12:00:37 +0600

    menu api added

--- src/redux/features/menu/menuApi.ts
+++ src/redux/features/menu/menuApi.ts
@@ -3,6 +3,26 @@ import { baseApi } from "@/redux/api/baseApi/baseApi";
 const menuApi = baseApi
           .injectEndpoints({
                     endpoints: (builder) => ({
+                              getAllMenu: builder.query({
+                                        query: () => "menus/all",
+                                        providesTags: ["Menu"],
+                              }),
+
+                              getSingleMenuByCategoryId: builder.query({
+                                        query: (query) => `menus/all?${query}`,
+                                        providesTags: ["Menu"],
+                              }),
+
+                              getMenusByCategory: builder.query({
+                                        query: (category) => `/menus/all?category=${category}&skipLimit=YES`,
+                                        providesTags: ["Menu"],
+                              }),
+
+                              getSingleMenuItemByID: builder.query({
+                                        query: (id) => `/menus/menu/${id}`,
+                                        providesTags: ["Menu"],
+                              }),
+
                               getAllCategories: builder.query({
                                         query: () => ({
                                                   url: "/categories/all-categories",
@@ -20,6 +40,10 @@ const menuApi = baseApi
           });
 
 export const {
+          useGetAllMenuQuery,
+          useGetSingleMenuByCategoryIdQuery,
+          useGetMenusByCategoryQuery,
+          useGetSingleMenuItemByIDQuery,
           useGetAllCategoriesQuery,
           useGetSingleCategoryQuery,
 } = menuApi;
