019cba66da33ac886cc31d3efc60f8e2458ffcf0
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2024-05-08 19:53:16 +0600

    scroll issue fixed

--- src/hooks/useScrollIntoView.tsx
+++ src/hooks/useScrollIntoView.tsx
@@ -1,20 +1,11 @@
 "use client"
 
-import { useSearchParams } from "next/navigation";
 import { RefObject, useEffect } from "react";
 
-export default function useScrollIntoView({ ref }: { ref: RefObject<HTMLDivElement> }) {
-          const searchparams = useSearchParams();
-
+export default function useScrollIntoView({ ref, searchparams }: { ref: RefObject<HTMLDivElement>, searchparams: URLSearchParams }) {
           useEffect(() => {
                     if (searchparams.toString() !== "") {
                               typeof window !== "undefined" && window.innerWidth > 768 && ref.current?.scrollIntoView({ behavior: "smooth", block: "center" });
                     }
           }, [searchparams, ref]);
-};
-
-export function ScrollIntoView({ ref }: { ref: RefObject<HTMLDivElement> }) {
-          useScrollIntoView({ ref });
-
-          return null;
 };
