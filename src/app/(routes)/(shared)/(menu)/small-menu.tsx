4d854537de052e8e62c92e93830b87bcae0b4c5e
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-08 22:29:44 +0600

    sm basic menu design

--- src/app/(routes)/(shared)/(menu)/small-menu.tsx
+++ src/app/(routes)/(shared)/(menu)/small-menu.tsx
@@ -26,56 +26,7 @@ export default function SmallMenu() {
             Staters
           </AccordionTrigger>
 
-          <AccordionContent className=" font-semibold text-xs border-b-2 mb-2">
-            Category Des: Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit.
-          </AccordionContent>
-
-          <AccordionContent className="border-b-2 mb-2">
-            <h5 className="font-bold text-[15px]">Chicken Tikka</h5>
-            <p className="text-xs font-semibold">
-              Item Des: Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit.
-            </p>
-
-            <div className="flex justify-end items-center space-x-4">
-              <p className="font-semibold">
-                {Config.currency}
-                {role === "admin" ? "4.20" : "3.80"}
-              </p>
-
-              <Button className="bg-destructive dark:bg-primary" size="sm">
-                <Fa6Icons.FaPlus className="text-xl text-primary-foreground" />
-              </Button>
-            </div>
-          </AccordionContent>
-
-          <AccordionContent className="border-b-2">
-            <h5 className="font-bold text-[15px]">Chicken Tikka</h5>
-            <p className="text-xs font-semibold">
-              Item Des: Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit.
-            </p>
-
-            <div className="flex justify-end items-center space-x-4">
-              <p className="font-semibold">
-                {Config.currency}
-                {role === "admin" ? "4.20" : "3.80"}
-              </p>
-
-              <Button className="bg-destructive dark:bg-primary" size="sm">
-                <Fa6Icons.FaPlus className="text-xl text-primary-foreground" />
-              </Button>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-
-        <AccordionItem className="border-b-2 border-primary/25" value="item-2">
-          <AccordionTrigger className="text-destructive dark:text-primary font-semibold text-lg">
-            Tandoori Dishes
-          </AccordionTrigger>
-
-          <AccordionContent className=" font-semibold text-xs border-b-2 mb-2">
+          <AccordionContent className=" font-semibold text-xs border-b mb-2">
             Category Des: Lorem ipsum, dolor sit amet consectetur adipisicing
             elit.
           </AccordionContent>
