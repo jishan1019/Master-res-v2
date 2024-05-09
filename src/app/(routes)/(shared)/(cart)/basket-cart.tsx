65931c9152c0cee1ae27a86873de6eaa7d683047
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-09 23:51:33 +0600

    basket responsive complete

--- src/app/(routes)/(shared)/(cart)/basket-cart.tsx
+++ src/app/(routes)/(shared)/(cart)/basket-cart.tsx
@@ -15,15 +15,17 @@ export default function BasketCart() {
       <main className="w-full mx-auto py-1 pl-2">
         <section>
           <div className="grid grid-cols-12 font-semibold text-[17px] text-destructive dark:text-primary-foreground gap-3 mt-3">
-            <p className="col-span-5">Food</p>
-            <p className="col-span-2 text-center">Price</p>
-            <p className="col-span-5 text-center">Qty</p>
+            <p className="col-span-5 md:col-span-6">Food</p>
+            <p className="col-span-2 md:hidden text-center">Price</p>
+            <p className="col-span-5 md:col-span-6 text-center">Qty</p>
           </div>
 
           <div className="grid grid-cols-12 text-sm gap-3 mt-2 font-semibold border-b-2 pb-2">
-            <p className="col-span-5">Chicken Tikka</p>
-            <p className="col-span-2 text-center">{Config.currency}4.20</p>
-            <div className="col-span-5 flex justify-center items-center gap-1 md:gap-2">
+            <p className="col-span-5 md:col-span-6">Chicken Tikka</p>
+            <p className="col-span-2 md:hidden text-center">
+              {Config.currency}4.20
+            </p>
+            <div className="col-span-5 md:col-span-6 flex justify-center items-center gap-1 sm:gap-2">
               <Button
                 className="bg-secondary-foreground/50 dark:bg-primary"
                 size="xs"
@@ -44,23 +46,27 @@ export default function BasketCart() {
           </div>
 
           <div className="grid grid-cols-12 font-bold text-sm gap-3 mt-2">
-            <p className="col-span-5">Food Total</p>
-            <p className="col-span-2 text-center">{Config.currency}20.45</p>
-            <p className="col-span-5 text-center">8</p>
+            <p className="col-span-5 md:col-span-6">Food Total</p>
+            <p className="col-span-2 md:col-span-6 text-center">
+              {Config.currency}20.45
+            </p>
+            <p className="col-span-5 md:hidden text-center">8</p>
           </div>
         </section>
 
         <section>
           <div className="grid grid-cols-12 font-semibold text-[17px] text-destructive dark:text-primary-foreground gap-3 mt-3">
-            <p className="col-span-5">Drinks</p>
-            <p className="col-span-2 text-center"></p>
-            <p className="col-span-5 text-center">Qty</p>
+            <p className="col-span-5 md:col-span-6">Drinks</p>
+            <p className="col-span-2 md:hidden text-center">Price</p>
+            <p className="col-span-5 md:col-span-6 text-center">Qty</p>
           </div>
 
           <div className="grid grid-cols-12 text-sm gap-3 mt-2 font-semibold border-b-2 pb-2">
-            <p className="col-span-5">Chicken Tikka</p>
-            <p className="col-span-2 text-center">{Config.currency}4.20</p>
-            <div className="col-span-5 flex justify-center items-center gap-1 md:gap-2">
+            <p className="col-span-5 md:col-span-6">Chicken Tikka</p>
+            <p className="col-span-2 md:hidden text-center">
+              {Config.currency}4.20
+            </p>
+            <div className="col-span-5 md:col-span-6 flex justify-center items-center gap-1 sm:gap-2">
               <Button
                 className="bg-secondary-foreground/50 dark:bg-primary"
                 size="xs"
@@ -70,7 +76,7 @@ export default function BasketCart() {
                   className="text-primary-foreground"
                 />
               </Button>
-              <p>5</p>
+              <p>50</p>
               <Button className="bg-destructive dark:bg-primary" size="xs">
                 <Fa6Icons.FaPlus
                   size={15}
@@ -81,16 +87,18 @@ export default function BasketCart() {
           </div>
 
           <div className="grid grid-cols-12 font-bold text-sm gap-3 mt-2">
-            <p className="col-span-5">Drinks Total</p>
-            <p className="col-span-2 text-center">{Config.currency}20.45</p>
-            <p className="col-span-5 text-center">8</p>
+            <p className="col-span-5 md:col-span-6">Drinks Total</p>
+            <p className="col-span-2 md:col-span-6 text-center">
+              {Config.currency}20.45
+            </p>
+            <p className="col-span-5 md:hidden text-center">8</p>
           </div>
         </section>
 
         <section>
-          <div className="grid grid-cols-12 font-bold text-[16px] gap-3 mt-3 mr-2">
+          <div className="grid grid-cols-12 font-bold text-[16px] gap-3 mt-3">
             <p className="col-span-6">Order Total</p>
-            <p className="col-span-6 text-end">{Config.currency}20.45</p>
+            <p className="col-span-6 text-center">{Config.currency}20.45</p>
           </div>
         </section>
       </main>
